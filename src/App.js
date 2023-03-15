dd743291ee5f5599afedfe1a6659d40e2ec73970
Gregor Hočevar <51193034+ghocevar@users.noreply.github.com>
2023-03-15 15:48:30 +0100

    Debug missing api url

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import "./App.css";
-import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import ReportPage from "./pages/ReportPage.jsx";
-import PreviousReportPage from "./pages/PreviousReportPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import InsertDataPage from "./pages/InsertDataPage.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import { ProtectedRoute } from "./router/ProtectedRouter.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import RegisterPage from "./pages/RegisterPage";
-import SetPasswordPage from "./pages/SetPasswordPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import UnauthorizedPage from "./pages/UnauthorizedPage.jsx";
+import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import ReportPage from './pages/ReportPage.jsx';
+import PreviousReportPage from './pages/PreviousReportPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import InsertDataPage from './pages/InsertDataPage.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+import { ProtectedRoute } from './router/ProtectedRouter.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage';
+import SetPasswordPage from './pages/SetPasswordPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import UnauthorizedPage from './pages/UnauthorizedPage.jsx';
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route path="/register" component={RegisterPage} />
-				<Route path="/insertdata" component={InsertDataPage} />
-				<Route path="/setPassword/:email" component={SetPasswordPage} />
-				<Route path="/report/:id" component={ReportPage} />
-				<Route path="/previousReports/:id" component={PreviousReportPage} />
-				<Route path="/forgotPassword" component={ForgotPasswordPage} />
-				<Route path="/404" component={PageNotFound} />
-				<Route path="/login" component={LoginPage} />
-				<Route path="/" component={HomePage} />
-				<Route path="/unauthorized" component={UnauthorizedPage} />
-				<Redirect to="/404" />
-			</Switch>
-		</Router>
-	);
+  console.log('REACT_APP_URL', process.env.REACT_APP_URL);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/insertdata" component={InsertDataPage} />
+        <Route path="/setPassword/:email" component={SetPasswordPage} />
+        <Route path="/report/:id" component={ReportPage} />
+        <Route path="/previousReports/:id" component={PreviousReportPage} />
+        <Route path="/forgotPassword" component={ForgotPasswordPage} />
+        <Route path="/404" component={PageNotFound} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={HomePage} />
+        <Route path="/unauthorized" component={UnauthorizedPage} />
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
