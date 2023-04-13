cb2abc92e6e85c9c15fcbe5a0ca4b86a016199f8
L <>
2023-04-13 11:35:50 +0200

    Push test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
-
-import { React, useEffect } from "react";
+import {React, useEffect} from "react";
 
 import Report from "../components/Report";
 import HomeDataContextProvider from "../contexts/HomeDataContext";
@@ -14,16 +13,17 @@ import SuccessModal from "../components/SuccessModal";
 import FailureModal from "../components/FailureModal";
 import TourData from "../components/TourData";
 import POIData from "../components/POIData";
+
 const HomePage = () => {
 
 	return (
-		<div>
+		<div class="border border-red-500">
 			<HomeDataContextProvider>
-				<TourData />
-				<POIData />
-				<SuccessModal />
-				<FailureModal />
-				<HomePageData />
+				<TourData/>
+				<POIData/>
+				<SuccessModal/>
+				<FailureModal/>
+				<HomePageData/>
 				<PreviousReportTourModal/>
 				<AddNewTourForm/>
 				<AddNewPartnerForm/>
