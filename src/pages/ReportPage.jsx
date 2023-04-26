948d3ead70944f195ccc0e3922df309865be7a8f
L <>
2023-04-26 14:39:26 +0200

    Partner page

--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,23 +1,21 @@
-
-import { React, useEffect } from "react";
+import {React, useEffect} from "react";
 
 import Report from "../components/Report";
 import ReportContextProvider from "../contexts/ReportContext";
 import ReportModal from "../components/ReportModal";
 import ReportPrint from "../components/ReportPrint";
 
-import { authHeader } from "../helpers/auth-header";
+import {authHeader} from "../helpers/auth-header";
 import Axios from "axios";
+
 var url = process.env.REACT_APP_URL || "http://localhost:8080/";
 const ReportPage = () => {
-
-
 
 
 	return (
 		<div>
 			<ReportContextProvider>
-				<ReportModal />
+				<ReportModal/>
 				<ReportPrint/>
 			</ReportContextProvider>
 		</div>
